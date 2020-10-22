81b1f1501e6156d62c5dd2a3d7c17681edc88e71
Ben Luddy <bluddy@redhat.com>
2020-10-22 14:16:45 -0400

    Phony test to see whether or not coverage reporting works.

--- cmd/internal/resolve/hacks_test.go
+++ cmd/internal/resolve/hacks_test.go
@@ -1,8 +1,12 @@
-package resolve_test
+package resolve
 
 import (
 	"testing"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 func TestIsAHack(t *testing.T) {
+	var b InputBuilder
+	b.Add(&unstructured.Unstructured{})
 }
